7ea6a367517fabea6a08f433d34375dde29299d9
Harris Mustic <harrismustic@gmail.com>
2019-10-09 14:52:48 -0700

    Created new file, CSE 360 Assignment 2

---
+++ AddingMachine.java
@@ -0,0 +1,36 @@
+// Assignment #: 2
+//         Name: Harris Mustic
+//    StudentID: 1209221893
+//  Lab Lecture: Wednesday 9:40-5:50
+//  Description: Learn how to use github
+
+package cse360assign2;
+
+public class AddingMachine {
+
+	private int total;
+	
+	public AddingMachine () {
+		total = 0;  // not needed - included for clarity
+	}
+	
+	public int getTotal () {
+		return 0;
+	}
+	
+	public void add (int value) {
+		
+	}
+	
+	public void subtract (int value) {
+		
+	}
+		
+	public String toString () {
+		return "";
+	}
+
+	public void clear() {
+	
+	}
+}
